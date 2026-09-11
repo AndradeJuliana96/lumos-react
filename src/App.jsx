dd54a265725a1475f4017a138383e932f21f90dd
AndradeJuliana96 <andrade96.juliana@gmail.com>
2026-09-11 01:23:56 -0300

    implementa estrutura do carrinho

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Link, Route, Routes } from 'react-router-dom';
+import Carrinho from './pages/Carrinho.jsx';
+import { produtos } from './data/produtos.js';
+
 export default function App() {
   return (
-    <main>
-      <h1>Lumos</h1>
-      <p>Minha loja de luminárias está em construção.</p>
-    </main>
+    <Routes>
+      <Route path="/" element={<Carrinho produtos={produtos} />} />
+      <Route path="/pagamento" element={
+        <main>
+          <h1>Pagamento em construção</h1>
+          <Link to="/">Voltar ao carrinho</Link>
+        </main>
+      } />
+    </Routes>
   );
 }
